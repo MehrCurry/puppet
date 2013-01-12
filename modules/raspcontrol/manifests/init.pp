05abc9ea5c63cf58ffdff624a4e8c5d010750bce
Guido Zockoll <guido@builder.fritz.box>
2013-01-12 08:05:43 +0100

    added raspcontrol

--- modules/raspcontrol/manifests/init.pp
+++ modules/raspcontrol/manifests/init.pp
@@ -1,16 +1,16 @@
 # Installs latest raspcontrol from github
 class rapscontrol {
-  excc: { 'git checkout'
+  excc { 'git checkout':
     command => 'cd /opt && git clone https://github.com/Bioshox/Raspcontrol.git'
     path => '/bin:/usr/bin'
     require => Package['php5-cli','git']
   }
 
-  file: { '/etc/init.d/raspcontol'
+  file { '/etc/init.d/raspcontol':
     source => 'puppet:///modules/raspcontrol/raspcontrol'
   }
 
-  service: { 'raspcontrol'
+  service { 'raspcontrol':
     ensure => running,
     enabled => true
   }
